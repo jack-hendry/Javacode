a8ed07d4661fac3902137b27468d3036cd9e5efa
jack-hendry <jack.r.hendry.p@gmail.com>
2025-07-17 16:57:03 -0400

    Methods

--- basics/Demo.java
+++ basics/Demo.java
@@ -1,26 +1,25 @@
 
- class Calculator {
-    int a;
-
-    public int add(int a, int b) {
-        System.out.println("Adding two numbers: " + a + " and " + b);
-        return a + b; // Method to add two integers
+class Computer {
+    public void playMusic() {
+        System.out.println("Playing music...");
+    }
+    public String getMeAPen(int cost){ 
+        if(cost >= 10) {
+        return "Pen";
+        }
+            return "Not enough money for a pen";
     }
 }
 
-    public class Demo extends Calculator {
+    public class Demo {
 
         
         public static void main(String[] args) {
-            // This is the main method where the program starts
-            int num1 = 4;
-            int num2 = 5;
-            Calculator calc = new Calculator();
-            // int result = num1 + num2; // Adding two integers
-            calc.add(num1, num2); // Using the add method from Calculator class
-            System.out.println("The result of addition is: "  + calc.add(num1, num2)); // Displaying the result
-            // System.out.println("Hello, World!"); // This line is commented ou
-
+          
+            Computer computer = new Computer();
+            computer.playMusic();
+            String str = computer.getMeAPen(0);
+            System.out.println(str);
         }
     }
 
