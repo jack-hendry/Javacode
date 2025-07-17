cc20a35c2a8829daee8ccccecc39e009be6ca518
jack-hendry <jack.r.hendry.p@gmail.com>
2025-07-17 13:45:06 -0400

    How Java works

---
+++ basics/Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    public static void main(String[] args) {
+        // This is the main method where the program starts
+        byte result = 127;
+        long l = 5854l;
+        float f = 5.8f;
+        System.out.println(result);
+    }
+}
